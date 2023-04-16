2ab35527cc7327af8a4ef7581a27c8a47fd30272
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-16 13:07:28 -0300

    feat: commit initial

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  clearMocks: true,
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
